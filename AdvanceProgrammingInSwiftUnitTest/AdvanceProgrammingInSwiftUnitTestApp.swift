b272f97e135d85f3bd552cc4718d25f4b21abd51
luu-9798 <trungluu9798@gmail.com>
2025-05-12 16:46:42 -0400

    Initial Commit

---
+++ AdvanceProgrammingInSwiftUnitTest/AdvanceProgrammingInSwiftUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceProgrammingInSwiftUnitTestApp.swift
+//  AdvanceProgrammingInSwiftUnitTest
+//
+//  Created by Trung Luu on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceProgrammingInSwiftUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
