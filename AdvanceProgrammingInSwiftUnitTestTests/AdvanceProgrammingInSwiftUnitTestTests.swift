f8c18078b51e7177e272932b79b87c36274c617e
luu-9798 <trungluu9798@gmail.com>
2025-05-12 17:02:10 -0400

    Arrange data by creating array of items and tax percentage constants

--- AdvanceProgrammingInSwiftUnitTestTests/AdvanceProgrammingInSwiftUnitTestTests.swift
+++ AdvanceProgrammingInSwiftUnitTestTests/AdvanceProgrammingInSwiftUnitTestTests.swift
@@ -14,6 +14,13 @@ final class AdvanceProgrammingInSwiftUnitTestTests: XCTestCase {
     
     //Create a test function for 20% of tax
     func test_calculateTotal_salesTaxTwentyPercent() {
-        
+        //Arrange data by creating array of items and tax percentage constants
+        let items = [
+            CheckoutItem(name: "Fish and Chips", price: 625),
+            CheckoutItem(name: "Cheeseburger", price: 850),
+            CheckoutItem(name: "Milkshake", price: 325),
+            CheckoutItem(name: "Black Coffee", price: 175),
+        ]
+        let taxPercentage = 20
     }
 }
